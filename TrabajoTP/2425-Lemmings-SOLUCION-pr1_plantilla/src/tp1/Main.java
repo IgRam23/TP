6352a0edbb4348a1c52ceb542d135812e29722df
IgRam23 <ignaciorammirez@gmail.com>
2024-10-09 16:23:02 +0200

    Actualizacion

--- TrabajoTP/2425-Lemmings-SOLUCION-pr1_plantilla/src/tp1/Main.java
+++ TrabajoTP/2425-Lemmings-SOLUCION-pr1_plantilla/src/tp1/Main.java
@@ -20,7 +20,7 @@ public class Main {
 	public static void main(String[] args) {
 		// Required to avoid issues with tests
 		Locale.setDefault(new Locale("es", "ES"));
-		
+		// Hola mundo
 		try {
 			
 			int nLevel = 1;
